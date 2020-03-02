4cde14db82c037aee8d02933da7687cbdf7cd161
charanjeetkau <ckbss97@gmail.com>
2020-03-02 18:19:00 +0530

    added: sum & avg of 2digit Random numbers

---
+++ random4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+R1=$(( $RANDOM % 89 + 10 ))
+R2=$(( $RANDOM % 89 + 10 ))
+R3=$(( $RANDOM % 89 + 10 ))
+R4=$(( $RANDOM % 89 + 10 ))
+R5=$(( $RANDOM % 89 + 10 ))
+
+sum=$(( $R1 + $R2 + $R3 + $R4 + $R5 ))
+echo "$R1 + $R2 + $R3 + $R4 + $R5 = " $sum
+
+avg=$(( $sum / 5 ))
+echo "Average= " $avg
+
